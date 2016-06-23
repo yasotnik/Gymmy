2201d7969a695c3dac03e049ec079b7e875d748c
levshkatov <l.shkatov@gmail.com>
2016-06-23 04:42:15 +0000

    Sockets away

--- rpi/light_sensor.py
+++ rpi/light_sensor.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import time
 import socket
 import db_actions
+import os
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -14,6 +15,7 @@ touch0 = False
 touch1 = False
 sock = socket.socket()
 sock.connect(('192.168.1.67', 9092))
+pathDBactions = os.path.abspath("../libs/db_actions.py")
     
 def rc_time (pinLDR):
     count = 0
@@ -39,31 +41,42 @@ def led(lh, pinLED):
 def touched(id, touch, pin1, data):
     if touch == False:
         led(0, pin1)
-        pin = d[str(data)]
-        led(1, pin)
+        if (str(data) = "B"):
+            led(1, pinLED1)
+        if (str(data) = ";"):
+            led(0, pinLED0)
+            led(0, pinLED1)
+        if (str(data) = "A"):
+            led(1, pinLED0)
         touch = True
         
 def detouched(touch):
     if touch == True:
-        touch = False    
+        touch = False
+
+def parse(id):
+    data = str(pathDBactions.get_map(easy))
+    dataArr = data.split(str=",")
+    return dataArr[id]
+
+def checkLDR():
+    if rc_time(pinLDR0) > 5000:
+        touched(0, touch0, pinLED0, parse(1))
+    else:
+        detouched(touch0)
+        if rc_time(pinLDR1) > 5000:
+            touched(1, touch1, pinLED1, parse(2))
+        else:
+            detouched(touch1)
 
 try:
     while True:
         time.sleep(0.5)
         print ("0: " + str(rc_time(pinLDR0)) + "   1: " + str(rc_time(pinLDR1)))
-        data_start = db_actions.get_status("start")
-        data_stop = db_actions.get_status("stop")
+        data_start = pathDBactions.get_status("start")
+        data_stop = pathDBactions.get_status("stop")
         if str(data_start) == "start":
-            if rc_time(pinLDR0) > 5000:
-                sock.send('0')
-                touched(0, touch0, pinLED0, sock.recv(256))
-            else:
-                detouched(touch0)
-            if rc_time(pinLDR1) > 5000:
-                sock.send('1')
-                touched(1, touch1, pinLED1, sock.recv(256))
-            else:
-                detouched(touch1)
+            checkLDR()            
         elif str(data_stop) == "stop":
             led(0, pinLED0)
             led(0, pinLED1)
